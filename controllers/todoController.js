688014db99a19d6e8f634047e928001698dd0100
1337NamNori <nam20051999@gmail.com>
2021-05-28 18:18:12 +0700

    New feature: Edit & Update item

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Todo from '../models/Todos.js';
 
 export const readTodos = async (req, res, next) => {
@@ -14,6 +15,22 @@ export const createTodo = async (req, res, next) => {
     const todo = new Todo(req.body);
     try {
         await todo.save();
+        res.status(201).json(todo);
+    } catch (err) {
+        console.log(err);
+        res.status(409).json({ error: err.message });
+    }
+};
+
+export const updateTodo = async (req, res, next) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(404).send('The ID is not valid');
+    }
+    const todo = { title, content, _id: id };
+    try {
+        await Todo.findByIdAndUpdate(id, todo, { new: true });
         res.status(201).json(todo);
     } catch (err) {
         console.log(err);
--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { readTodos, createTodo } from '../controllers/todoController.js';
+import {
+    readTodos,
+    createTodo,
+    updateTodo,
+} from '../controllers/todoController.js';
 const router = express.Router();
 
 router.get('/', readTodos);
 router.post('/', createTodo);
+router.put('/:id', updateTodo);
 
 export default router;
